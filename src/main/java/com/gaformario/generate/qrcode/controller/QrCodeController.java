512891f47ab1e5e30231f845256d009702820f23
Gabriel Formario <49768089+gaformario@users.noreply.github.com>
2025-06-19 19:53:12 -0300

    fix: import http status

--- src/main/java/com/gaformario/generate/qrcode/controller/QrCodeController.java
+++ src/main/java/com/gaformario/generate/qrcode/controller/QrCodeController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
